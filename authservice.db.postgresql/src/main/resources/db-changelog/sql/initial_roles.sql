64e704dbd55be23ac74b22f2e7ba726f202a14de
Steinar Bang <sb@dod.no>
2019-04-11 19:54:01 +0200

    Remove initial admin user and role from the authservice PostgreSQL database, since this is now set up by the authservice liquibase bundle

--- authservice.db.postgresql/src/main/resources/db-changelog/sql/initial_roles.sql
+++
@@ -1,4 +0,0 @@
---liquibase formatted sql
---changeset sb:initial_roles
-insert into roles (role_name, description) values ('useradmin','Administrate users');
---rollback delete from roles; alter table roles alter role_id restart with 1;
--- authservice.db.postgresql/src/main/resources/db-changelog/sql/initial_user_roles.sql
+++
@@ -1,4 +0,0 @@
---liquibase formatted sql
---changeset sb:initial_user_roles
-insert into user_roles (role_name, username) values ('useradmin','admin');
---rollback delete from user_roles; alter table user_role alter user_role_id restart with 1;
