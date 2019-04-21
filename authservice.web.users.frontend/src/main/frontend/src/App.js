5da1698071cb8781620e7a488d67a7029ebd9902
Steinar Bang <sb@dod.no>
2019-04-21 20:54:42 +0200

    Set state on the <Provider> in the simplest way possible

--- authservice.web.users.frontend/src/main/frontend/src/App.js
+++ authservice.web.users.frontend/src/main/frontend/src/App.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import { Switch, Route, BrowserRouter as Router, NavLink } from 'react-router-dom';
-import { Provider } from 'react-redux';
 import logo from './logo.svg';
 import './App.css';
 import Home from './components/Home';
@@ -18,32 +17,25 @@ import PermissionModify from './components/PermissionModify';
 import PermissionAdd from './components/PermissionAdd';
 
 class App extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {...props};
-    }
-
     render() {
         return (
-            <Provider store={this.state.store}>
-                <Router>
-                    <Switch>
-                        <Route exact path="/authservice/useradmin/" component={Home} />
-                        <Route path="/authservice/useradmin/users/modify" component={UserModify} />
-                        <Route path="/authservice/useradmin/users/roles" component={UserRoles} />
-                        <Route path="/authservice/useradmin/users/add" component={UserAdd} />
-                        <Route path="/authservice/useradmin/users/passwords" component={UserChangePasswords} />
-                        <Route path="/authservice/useradmin/users" component={Users} />
-                        <Route path="/authservice/useradmin/roles/modify" component={RoleModify} />
-                        <Route path="/authservice/useradmin/roles/permissions" component={RolePermissions} />
-                        <Route path="/authservice/useradmin/roles/add" component={RoleAdd} />
-                        <Route path="/authservice/useradmin/roles" component={Roles} />
-                        <Route path="/authservice/useradmin/permissions/modify" component={PermissionModify} />
-                        <Route path="/authservice/useradmin/permissions/add" component={PermissionAdd} />
-                        <Route path="/authservice/useradmin/permissions" component={Permissions} />
-                    </Switch>
-                </Router>
-            </Provider>
+            <Router>
+                <Switch>
+                    <Route exact path="/authservice/useradmin/" component={Home} />
+                    <Route path="/authservice/useradmin/users/modify" component={UserModify} />
+                    <Route path="/authservice/useradmin/users/roles" component={UserRoles} />
+                    <Route path="/authservice/useradmin/users/add" component={UserAdd} />
+                    <Route path="/authservice/useradmin/users/passwords" component={UserChangePasswords} />
+                    <Route path="/authservice/useradmin/users" component={Users} />
+                    <Route path="/authservice/useradmin/roles/modify" component={RoleModify} />
+                    <Route path="/authservice/useradmin/roles/permissions" component={RolePermissions} />
+                    <Route path="/authservice/useradmin/roles/add" component={RoleAdd} />
+                    <Route path="/authservice/useradmin/roles" component={Roles} />
+                    <Route path="/authservice/useradmin/permissions/modify" component={PermissionModify} />
+                    <Route path="/authservice/useradmin/permissions/add" component={PermissionAdd} />
+                    <Route path="/authservice/useradmin/permissions" component={Permissions} />
+                </Switch>
+            </Router>
         );
     }
 }
--- authservice.web.users.frontend/src/main/frontend/src/index.js
+++ authservice.web.users.frontend/src/main/frontend/src/index.js
@@ -4,13 +4,18 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import { configureStore } from 'redux-starter-kit';
+import { Provider } from 'react-redux';
 import rootReducer from './reducers';
 
 const store = configureStore({
   reducer: rootReducer
 });
 
-ReactDOM.render(<App store={store} />, document.getElementById('root'));
+ReactDOM.render(
+    <Provider store={store}>
+        <App/>
+    </Provider>,
+    document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
