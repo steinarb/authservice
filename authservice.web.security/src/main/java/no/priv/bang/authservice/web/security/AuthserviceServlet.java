a4801c76ff4bd0f693d30cb190767f6aa1e806fa
Steinar Bang <sb@dod.no>
2019-07-03 21:12:21 +0200

    Fix copy/paste leftovers in log messages

--- authservice.web.security/src/main/java/no/priv/bang/authservice/web/security/AuthserviceServlet.java
+++ authservice.web.security/src/main/java/no/priv/bang/authservice/web/security/AuthserviceServlet.java
@@ -86,7 +86,7 @@ public class AuthserviceServlet extends ServletContainer {
         reload(copyOfExistingConfig);
         Map<String, Object> configProperties = getConfiguration().getProperties();
         Set<Class<?>> classes = getConfiguration().getClasses();
-        logservice.log(LogService.LOG_INFO, String.format("Ukelonn Jersey servlet initialized with WebConfig, with resources: %s  and config params: %s", classes.toString(), configProperties.toString()));
+        logservice.log(LogService.LOG_INFO, String.format("Authservice Jersey servlet initialized with WebConfig, with resources: %s  and config params: %s", classes.toString(), configProperties.toString()));
     }
 
 }
