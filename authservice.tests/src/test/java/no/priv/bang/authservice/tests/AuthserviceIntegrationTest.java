f5459f2331a297e43332d7edc751fae1da185da3
Steinar Bang <sb@dod.no>
2020-12-31 17:18:46 +0100

    Make authservice integration test be based on KarafTestSupport

--- authservice.tests/src/test/java/no/priv/bang/authservice/tests/AuthserviceIntegrationTest.java
+++ authservice.tests/src/test/java/no/priv/bang/authservice/tests/AuthserviceIntegrationTest.java
@@ -1,89 +1,36 @@
 package no.priv.bang.authservice.tests;
 
-import static org.junit.Assert.*;
 import static org.ops4j.pax.exam.CoreOptions.*;
 import static org.ops4j.pax.exam.karaf.options.KarafDistributionOption.*;
 
-import java.io.File;
-import java.io.IOException;
-import java.net.ServerSocket;
-import java.net.URL;
+import java.util.stream.Stream;
 
-import javax.inject.Inject;
-import javax.servlet.Servlet;
-
+import org.apache.karaf.itests.KarafTestSupport;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.ops4j.pax.exam.Configuration;
 import org.ops4j.pax.exam.Option;
 import org.ops4j.pax.exam.junit.PaxExam;
-import org.ops4j.pax.exam.karaf.options.LogLevelOption.LogLevel;
 import org.ops4j.pax.exam.options.MavenArtifactUrlReference;
 import org.ops4j.pax.exam.spi.reactors.ExamReactorStrategy;
 import org.ops4j.pax.exam.spi.reactors.PerClass;
 
 @RunWith(PaxExam.class)
 @ExamReactorStrategy(PerClass.class)
-public class AuthserviceIntegrationTest {
-    public static final String RMI_SERVER_PORT = "44445";
-    public static final String RMI_REG_PORT = "1100";
-
-    @Inject
-    private Servlet authserviceServlet;
+public class AuthserviceIntegrationTest extends KarafTestSupport {
 
     @Configuration
     public Option[] config() {
-        final String jmxPort = freePortAsString();
-        final String httpPort = freePortAsString();
-        final String httpsPort = freePortAsString();
-        final MavenArtifactUrlReference karafUrl = maven().groupId("org.apache.karaf").artifactId("apache-karaf-minimal").type("zip").versionAsInProject();
-        final MavenArtifactUrlReference paxJdbcRepo = maven().groupId("org.ops4j.pax.jdbc").artifactId("pax-jdbc-features").versionAsInProject().type("xml").classifier("features");
         final MavenArtifactUrlReference authserviceFeatureRepo = maven().groupId("no.priv.bang.authservice").artifactId("authservice").version("LATEST").type("xml").classifier("features");
-        return options(
-            karafDistributionConfiguration().frameworkUrl(karafUrl).unpackDirectory(new File("target/exam")).useDeployFolder(false).runEmbedded(true),
-            configureConsole().ignoreLocalConsole().ignoreRemoteShell(),
-            systemTimeout(720000),
-            keepRuntimeFolder(),
-            logLevel(LogLevel.DEBUG),
-            editConfigurationFilePut("etc/org.apache.karaf.management.cfg", "rmiRegistryPort", RMI_REG_PORT),
-            editConfigurationFilePut("etc/org.apache.karaf.management.cfg", "rmiServerPort", RMI_SERVER_PORT),
-            editConfigurationFilePut("etc/org.ops4j.pax.web.cfg", "org.osgi.service.http.port", httpPort),
-            editConfigurationFilePut("etc/org.ops4j.pax.web.cfg", "org.osgi.service.http.port.secure", httpsPort),
-            replaceConfigurationFile("etc/org.ops4j.pax.logging.cfg", getConfigFile("/etc/org.ops4j.pax.logging.cfg")),
-            systemProperty("org.ops4j.pax.logging.DefaultSer‌​viceLog.level").value("DEBUG"),
-            vmOptions("-Dtest-jmx-port=" + jmxPort),
-            junitBundles(),
-            features(paxJdbcRepo),
-            features(authserviceFeatureRepo, "user-admin-with-derby"));
+        Option[] options = new Option[] {
+            features(authserviceFeatureRepo)
+        };
+        return Stream.of(super.config(), options).flatMap(Stream::of).toArray(Option[]::new);
     }
 
     @Test
-    public void testAuthserviceServlet() {
-        // Verify that the service could be injected
-        assertNotNull(authserviceServlet);
-    }
-
-    static int freePort() {
-        try (final ServerSocket serverSocket = new ServerSocket(0)) {
-            serverSocket.setReuseAddress(true);
-            final int port = serverSocket.getLocalPort();
-
-            return port;
-        } catch (final IOException e) {
-            throw new IllegalStateException(e);
-        }
-    }
-
-    static String freePortAsString() {
-        return Integer.toString(freePort());
-    }
-
-    public File getConfigFile(String path) {
-        URL res = this.getClass().getResource(path);
-        if (res == null) {
-            throw new RuntimeException("Config resource " + path + " not found");
-        }
-        return new File(res.getFile());
+    public void testLoadFeature() throws Exception { // NOSONAR this test has an assert, just not an assert sonar recognizes
+        installAndAssertFeature("user-admin-with-testdb");
     }
 
 }
