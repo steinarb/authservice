5cc3628473a8fb3e4ea6561d936611d94511bd46
Steinar Bang <sb@dod.no>
2021-01-05 00:39:36 +0100

    Simplify the rootsReducer of authservice user admin

--- authservice.web.users.frontend/src/main/frontend/src/reducers/index.js
+++ authservice.web.users.frontend/src/main/frontend/src/reducers/index.js
@@ -1,37 +1,37 @@
 import { combineReducers } from 'redux';
 import { emptyUser, emptyUserAndPasswords, emptyRole, emptyPermission } from '../constants';
-import usersReducer from './usersReducer';
-import userReducer from './userReducer';
+import users from './usersReducer';
+import user from './userReducer';
 import userroles from './userrolesReducer';
 import rolesNotOnUser from './rolesNotOnUserReducer';
 import selectedInRolesNotOnUser from './selectedInRolesNotOnUserReducer';
 import rolesOnUser from './rolesOnUserReducer';
 import selectedInRolesOnUser from './selectedInRolesOnUserReducer';
-import passwordsReducer from './passwordsReducer';
-import rolesReducer from './rolesReducer';
-import roleReducer from './roleReducer';
-import rolepermissionsReducer from './rolepermissionsReducer';
-import permissionsReducer from './permissionsReducer';
-import permissionReducer from './permissionReducer';
-import formfieldReducer from './formfieldReducer';
-import errorsReducer from './errorsReducer';
+import passwords from './passwordsReducer';
+import roles from './rolesReducer';
+import role from './roleReducer';
+import rolepermissions from './rolepermissionsReducer';
+import permissions from './permissionsReducer';
+import permission from './permissionReducer';
+import formfield from './formfieldReducer';
+import errors from './errorsReducer';
 
 const rootsReducer = combineReducers({
-    users: usersReducer,
-    user: userReducer,
+    users,
+    user,
     userroles,
     rolesNotOnUser,
     selectedInRolesNotOnUser,
     rolesOnUser,
     selectedInRolesOnUser,
-    passwords: passwordsReducer,
-    roles: rolesReducer,
-    role: roleReducer,
-    rolepermissions: rolepermissionsReducer,
-    permissions: permissionsReducer,
-    permission: permissionReducer,
-    formfield: formfieldReducer,
-    errors: errorsReducer,
+    passwords,
+    roles,
+    role,
+    rolepermissions,
+    permissions,
+    permission,
+    formfield,
+    errors,
 });
 
 export default rootsReducer;
