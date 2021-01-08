d51f5f52563308dc35730cc99a1904404ed91342
Steinar Bang <sb@dod.no>
2021-01-08 19:16:14 +0100

    Deselect user roles select boxes after adding or removing a role on a user

--- authservice.web.users.frontend/src/main/frontend/src/reducers/selectedInRolesNotOnUserReducer.js
+++ authservice.web.users.frontend/src/main/frontend/src/reducers/selectedInRolesNotOnUserReducer.js
@@ -3,11 +3,11 @@ import {
     ROLES_NOT_ON_USER_SELECTED,
     ROLES_NOT_ON_USER_CLEAR,
 } from '../actiontypes';
-import { emptyPermission } from '../constants';
+import { emptyRole } from '../constants';
 
-const selectedInRolesNotOnUserReducer = createReducer(emptyPermission.id, {
+const selectedInRolesNotOnUserReducer = createReducer(emptyRole.id, {
     [ROLES_NOT_ON_USER_SELECTED]: (state, action) => action.payload,
-    [ROLES_NOT_ON_USER_CLEAR]: (state, action) => emptyPermission.id,
+    [ROLES_NOT_ON_USER_CLEAR]: (state, action) => emptyRole.id,
 });
 
 export default selectedInRolesNotOnUserReducer;
--- authservice.web.users.frontend/src/main/frontend/src/reducers/selectedInRolesOnUserReducer.js
+++ authservice.web.users.frontend/src/main/frontend/src/reducers/selectedInRolesOnUserReducer.js
@@ -1,10 +1,13 @@
 import { createReducer } from '@reduxjs/toolkit';
 import {
     ROLES_ON_USER_SELECTED,
+    ROLES_ON_USER_CLEAR,
 } from '../actiontypes';
+import { emptyRole } from '../constants';
 
-const selectedInRolesOnUserReducer = createReducer(-1, {
+const selectedInRolesOnUserReducer = createReducer(emptyRole.id, {
     [ROLES_ON_USER_SELECTED]: (state, action) => action.payload,
+    [ROLES_ON_USER_CLEAR]: (state, action) => emptyRole.id,
 });
 
 export default selectedInRolesOnUserReducer;
