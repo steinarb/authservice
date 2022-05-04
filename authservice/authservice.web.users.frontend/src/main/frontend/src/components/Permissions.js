89fab7c923f39e8ebd0da609ccb43faf520677e5
Steinar Bang <sb@dod.no>
2022-05-04 22:04:13 +0200

    Remove unused redux code from the Permissions react component

--- authservice/authservice.web.users.frontend/src/main/frontend/src/components/Permissions.js
+++ authservice/authservice.web.users.frontend/src/main/frontend/src/components/Permissions.js
@@ -1,13 +1,9 @@
 import React from 'react';
-import { connect } from 'react-redux';
-import {
-    PERMISSIONS_REQUEST,
-} from '../actiontypes';
 import Container from './bootstrap/Container';
 import StyledLinkLeft from './bootstrap/StyledLinkLeft';
 import StyledLinkRight from './bootstrap/StyledLinkRight';
 
-function Permissions() {
+export default function Permissions() {
     return (
         <div>
             <nav className="navbar navbar-light bg-light">
@@ -22,15 +18,3 @@ function Permissions() {
         </div>
     );
 }
-
-const mapStateToProps = (state) => {
-    return { ...state };
-};
-
-const mapDispatchToProps = dispatch => {
-    return {
-        onPermissions: () => dispatch(PERMISSIONS_REQUEST()),
-    };
-};
-
-export default connect(mapStateToProps, mapDispatchToProps)(Permissions);
