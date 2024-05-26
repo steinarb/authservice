1d49451e53251f6ac42024aebdb891a56283a97d
Steinar Bang <sb@dod.no>
2024-05-26 08:51:38 +0200

    Manually create PassThruAuthenticationFilter because the shiro 2.x INI parser can't find and create it when running in OSGi

--- authservice/authservice.web.security/src/main/java/no/priv/bang/authservice/web/security/AuthserviceShiroFilter.java
+++ authservice/authservice.web.security/src/main/java/no/priv/bang/authservice/web/security/AuthserviceShiroFilter.java
@@ -22,6 +22,7 @@ import org.apache.shiro.config.Ini;
 import org.apache.shiro.realm.Realm;
 import org.apache.shiro.session.mgt.eis.SessionDAO;
 import org.apache.shiro.web.env.IniWebEnvironment;
+import org.apache.shiro.web.filter.authc.PassThruAuthenticationFilter;
 import org.apache.shiro.web.mgt.DefaultWebSecurityManager;
 import org.apache.shiro.web.servlet.AbstractShiroFilter;
 import org.apache.shiro.web.session.mgt.DefaultWebSessionManager;
@@ -64,6 +65,7 @@ public class AuthserviceShiroFilter extends AbstractShiroFilter { // NOSONAR
     @Activate
     public void activate() {
         var environment = new IniWebEnvironment();
+        environment.getObjects().put("authc", new PassThruAuthenticationFilter());
         environment.setIni(INI_FILE);
         environment.setServletContext(getServletContext());
         environment.init();
