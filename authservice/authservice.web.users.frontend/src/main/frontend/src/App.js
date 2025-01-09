6d235fcbb38a282c63260984b6875a732bfab942
Steinar Bang <sb@dod.no>
2025-01-09 20:50:04 +0100

    Convert component App from class to functional component

--- authservice/authservice.web.users.frontend/src/main/frontend/src/App.js
+++ authservice/authservice.web.users.frontend/src/main/frontend/src/App.js
@@ -15,28 +15,24 @@ import Permissions from './components/Permissions';
 import PermissionModify from './components/PermissionModify';
 import PermissionAdd from './components/PermissionAdd';
 
-class App extends Component {
-    render() {
-        return (
-            <Router>
-                <Routes>
-                    <Route exact path="/authservice/useradmin/" element={<Home/>} />
-                    <Route path="/authservice/useradmin/users/modify" element={<UserModify/>} />
-                    <Route path="/authservice/useradmin/users/roles" element={<UserRoles/>} />
-                    <Route path="/authservice/useradmin/users/add" element={<UserAdd/>} />
-                    <Route path="/authservice/useradmin/users/passwords" element={<UserChangePasswords/>} />
-                    <Route path="/authservice/useradmin/users" element={<Users/>} />
-                    <Route path="/authservice/useradmin/roles/modify" element={<RoleModify/>} />
-                    <Route path="/authservice/useradmin/roles/permissions" element={<RolePermissions/>} />
-                    <Route path="/authservice/useradmin/roles/add" element={<RoleAdd/>} />
-                    <Route path="/authservice/useradmin/roles" element={<Roles/>} />
-                    <Route path="/authservice/useradmin/permissions/modify" element={<PermissionModify/>} />
-                    <Route path="/authservice/useradmin/permissions/add" element={<PermissionAdd/>} />
-                    <Route path="/authservice/useradmin/permissions" element={<Permissions/>} />
-                </Routes>
-            </Router>
-        );
-    }
+export default function App {
+    return (
+        <Router>
+            <Routes>
+                <Route exact path="/authservice/useradmin/" element={<Home/>} />
+                <Route path="/authservice/useradmin/users/modify" element={<UserModify/>} />
+                <Route path="/authservice/useradmin/users/roles" element={<UserRoles/>} />
+                <Route path="/authservice/useradmin/users/add" element={<UserAdd/>} />
+                <Route path="/authservice/useradmin/users/passwords" element={<UserChangePasswords/>} />
+                <Route path="/authservice/useradmin/users" element={<Users/>} />
+                <Route path="/authservice/useradmin/roles/modify" element={<RoleModify/>} />
+                <Route path="/authservice/useradmin/roles/permissions" element={<RolePermissions/>} />
+                <Route path="/authservice/useradmin/roles/add" element={<RoleAdd/>} />
+                <Route path="/authservice/useradmin/roles" element={<Roles/>} />
+                <Route path="/authservice/useradmin/permissions/modify" element={<PermissionModify/>} />
+                <Route path="/authservice/useradmin/permissions/add" element={<PermissionAdd/>} />
+                <Route path="/authservice/useradmin/permissions" element={<Permissions/>} />
+            </Routes>
+        </Router>
+    );
 }
-
-export default App;
