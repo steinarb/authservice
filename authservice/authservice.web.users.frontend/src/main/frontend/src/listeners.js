132a1242728bf8c251a36cb74492349eb0bbbd90
Steinar Bang <sb@dod.no>
2025-01-14 19:58:45 +0100

    Reload react app to let shiro decide the login state on 401 or 403 from API calls

--- authservice/authservice.web.users.frontend/src/main/frontend/src/listeners.js
+++ authservice/authservice.web.users.frontend/src/main/frontend/src/listeners.js
@@ -1,7 +1,19 @@
 import { createListenerMiddleware, isAnyOf } from '@reduxjs/toolkit';
 import { setPassword1, setPassword2, setPasswordsNotIdentical } from './reducers/passwordSlice';
+import { isFailedRequest } from './matchers';
 
 const listeners = createListenerMiddleware();
+
+listeners.startListening({
+    matcher: isFailedRequest,
+    effect: ({ payload }) => {
+        const { originalStatus } = payload || {};
+        const statusCode = parseInt(originalStatus);
+        if (statusCode === 401 || statusCode === 403) {
+            location.reload(true); // Will return to current location after the login process
+        }
+    }
+})
 
 listeners.startListening({
     matcher: isAnyOf(setPassword1, setPassword2),
