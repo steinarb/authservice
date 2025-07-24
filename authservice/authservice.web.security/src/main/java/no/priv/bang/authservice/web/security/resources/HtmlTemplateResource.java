67a1fc061f786188dff405a073d0c36ce0f1e94d
Steinar Bang <sb@dod.no>
2025-07-24 14:11:29 +0200

    Use var for inputstream in try-with-resource when reading classpath resource

--- authservice/authservice.web.security/src/main/java/no/priv/bang/authservice/web/security/resources/HtmlTemplateResource.java
+++ authservice/authservice.web.security/src/main/java/no/priv/bang/authservice/web/security/resources/HtmlTemplateResource.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2019-2024 Steinar Bang
+ * Copyright 2019-2025 Steinar Bang
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -31,7 +31,7 @@ public class HtmlTemplateResource {
     }
 
     protected Document loadHtmlFile(String htmlFile, LogService logservice) {
-        try (InputStream body = getClasspathResource(htmlFile)) {
+        try (var body = getClasspathResource(htmlFile)) {
             return Jsoup.parse(body, "UTF-8", "");
         } catch (IOException e) {
             var logger = logservice.getLogger(getClass());
